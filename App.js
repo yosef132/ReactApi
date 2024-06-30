04f7a7a0319ce1308145ea86dbe4c62838bdf2ba
yosef132 <123155868+yosef132@users.noreply.github.com>
2024-06-30 20:57:59 +0300

    Hi bisher

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Come on</Text>
+        <Text>Israel</Text>
+        <Text>Israel</Text>
+        <Text>Israel</Text>
+        <Text style={styles.text2}>Israel</Text>
+      </View>
+    </PaperProvider>
   );
 }
 
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  },
+  text2: {
+    color: 'blue',
   },
 });
